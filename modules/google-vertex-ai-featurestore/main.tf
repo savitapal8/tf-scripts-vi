515c7dc93927a65638bb1c07f7dff12aa78534fc
Savita Pal <savita.pal08@gmail.com>
2022-08-02 18:41:24 +0530

    Update main.tf

--- modules/google-vertex-ai-featurestore/main.tf
+++ modules/google-vertex-ai-featurestore/main.tf
@@ -2,7 +2,7 @@
 resource "google_vertex_ai_featurestore" "featurestore" {
   provider = google-beta
   name     = var.featurestore_name
-  labels = var.featurestore_labels
+  labels = var.labels
   region   = var.featurestore_region
   online_serving_config {
     fixed_node_count = var.node_count
