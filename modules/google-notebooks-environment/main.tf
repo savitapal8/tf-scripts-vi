f53985ca31e1899cc3ab569c1e04c6b389734e3d
Savita Pal <savita.pal08@gmail.com>
2022-08-02 18:37:47 +0530

    Update main.tf

--- modules/google-notebooks-environment/main.tf
+++ modules/google-notebooks-environment/main.tf
@@ -1,7 +1,8 @@
 #Google Notebooks Env
 resource "google_notebooks_environment" "environment" {
   name = var.notebooks_env_name
-  location = var.notebooks_zone 
+  location = var.notebooks_zone
+  labels = var.labels
   container_image {
     repository = var.image_repository
   } 
