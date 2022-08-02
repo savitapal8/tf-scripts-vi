39466f9f019463d5115438143115f0de1a30f327
Savita Pal <savita.pal08@gmail.com>
2022-08-02 18:40:28 +0530

    Update main.tf

--- modules/google-vertex-ai-dataset/main.tf
+++ modules/google-vertex-ai-dataset/main.tf
@@ -3,6 +3,7 @@ resource "google_vertex_ai_dataset" "dataset" {
   display_name          = var.ai_dataset_name
   metadata_schema_uri   = var.metadata_schema_uri
   region                = var.region
+  labels                = var.labels
   encryption_spec{
       kms_key_name = var.kms_key_id
   }
